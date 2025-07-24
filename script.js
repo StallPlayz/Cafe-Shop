fad91b2029ffb4a8d3467d25cce1ec96e3317f12
StallPlayz <stallplayz123@gmail.com>
2025-07-24 13:23:32 +0700

    A little bit of cleaning

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ feather.replace();
 
 // Toggle class active
 
-const navigation = document.querySelector('.navigation');
+const navigation = document.querySelector(".navigation");
 
 // Hamburger menu clicked
 
-document.querySelector('#hamburger-menu') . onclick = () => {
-    navigation.classList.toggle('active');
+document.querySelector("#hamburger-menu").onclick = () => {
+  navigation.classList.toggle("active");
 };
 
 // Click outside sidebar to close nav
 
-const hamburger = document.querySelector('#hamburger-menu');
+const hamburger = document.querySelector("#hamburger-menu");
 
-document.addEventListener('click', function(e){
-    if(!hamburger.contains(e.target) && !navigation.contains(e.target)) {
-        navigation.classList.remove('active');
-    }
-})
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navigation.contains(e.target)) {
+    navigation.classList.remove("active");
+  }
+});
