dfa56362debd1fc68aceceb8a32e7741b1f56557
StallPlayz <stallplayz123@gmail.com>
2025-07-24 12:06:08 +0700

    Adding background, device responsiveness, hamburger menu, etc

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-//icon
+// icon
 
 feather.replace();
+
+// Toggle class active
+
+const navigation = document.querySelector('.navigation');
+
+// Hamburger menu clicked
+
+document.querySelector('#hamburger-menu') . onclick = () => {
+    navigation.classList.toggle('active');
+};
+
+// Click outside sidebar to close nav
+
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+    if(!hamburger.contains(e.target) && !navigation.contains(e.target)) {
+        navigation.classList.remove('active');
+    }
+})
